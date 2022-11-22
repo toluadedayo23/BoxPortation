4b5ade0c9031568efabf1d94ff436f04b4f875c9
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-22 23:49:13 +0100

    Repository

--- src/main/java/com/boxportation/repository/BoxRepository.java
+++ src/main/java/com/boxportation/repository/BoxRepository.java
@@ -1,2 +1,8 @@
-package com.boxportation.repository;public interface BoxRepository {
+package com.boxportation.repository;
+
+import com.boxportation.model.Box;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoxRepository extends JpaRepository<Box, Long> {
+
 }
