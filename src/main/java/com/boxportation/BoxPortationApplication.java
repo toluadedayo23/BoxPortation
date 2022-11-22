71826c658f5b2c136ee90006a0ca11f293e0953e
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-22 21:43:20 +0100

    initial commit

---
+++ src/main/java/com/boxportation/BoxPortationApplication.java
@@ -0,0 +1,13 @@
+package com.boxportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxPortationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxPortationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boxportation/BoxPortationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxPortationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
