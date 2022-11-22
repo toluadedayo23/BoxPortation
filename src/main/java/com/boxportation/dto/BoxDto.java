90decb6db24d861158964526b5be727a33d553ae
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-22 23:47:26 +0100

    dto

--- src/main/java/com/boxportation/dto/BoxDto.java
+++ src/main/java/com/boxportation/dto/BoxDto.java
@@ -1,2 +1,21 @@
-package com.boxportation.dto;public class BoxDto {
+package com.boxportation.dto;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class BoxDto {
+
+    @NotBlank
+    @Size(max=20, message = "txref cannot be more than 20 charaters")
+    private String txref;
+
+    @Range(max = 500, message = "weight cannot be more than 500gr")
+    private Double weightLimit;
+
+    @Range(min = 0, max = 100, message = "Battery capacity ranges between 0 and 100")
+    private Double batteryCapacity;
 }
--- src/main/java/com/boxportation/dto/ItemsDto.java
+++ src/main/java/com/boxportation/dto/ItemsDto.java
@@ -1,2 +1,20 @@
-package com.boxportation.dto;public class ItemsDto {
+package com.boxportation.dto;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public class ItemsDto {
+
+    @NotBlank(message = "Item name cannot be blank")
+    @Pattern(regexp = "^[A-Za-z0-9-_]+$", message = "allowed only letters, numbers, ‘-‘, ‘_’")
+    private String name;
+
+    @DecimalMin("0.1")
+    private Double weight;
+
+    @NotBlank(message = "Item code cannot be blank")
+    @Pattern(regexp = "^[A-Z0-9_]+$")
+    private String code;
+
 }
