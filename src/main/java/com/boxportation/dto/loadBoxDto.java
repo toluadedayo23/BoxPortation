9af8a8808014ab4dfadab07e5e57c34fa174b807
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-23 01:55:31 +0100

    Dto

--- src/main/java/com/boxportation/dto/loadBoxDto.java
+++ src/main/java/com/boxportation/dto/loadBoxDto.java
@@ -1,2 +1,19 @@
-package com.boxportation.dto;public class loadBoxDto {
+package com.boxportation.dto;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class loadBoxDto {
+
+
+    @NotBlank
+    @Size(max=20, message = "txref cannot be more than 20 charaters")
+    private String txref;
+
+    @Range(min = 1, message = "incorrect Item Id")
+    private Long itemId;
 }
