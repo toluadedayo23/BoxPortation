df8541cc80fff1c3e9a87df8f75dd02c40968985
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-23 17:04:21 +0100

    Repository

--- src/main/java/com/boxportation/repository/BoxRepository.java
+++ src/main/java/com/boxportation/repository/BoxRepository.java
@@ -3,6 +3,7 @@ package com.boxportation.repository;
 import com.boxportation.model.Box;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoxRepository extends JpaRepository<Box, Long> {
@@ -10,4 +11,6 @@ public interface BoxRepository extends JpaRepository<Box, Long> {
     Optional<Box> findByTxref(String txref);
 
     boolean existsByTxref(String txref);
+
+    List<Box>  getBoxesByStateContainsAndBatteryCapacityGreaterThanEqual(String state, Double capacity);
 }
--- src/main/java/com/boxportation/repository/ItemRepository.java
+++ src/main/java/com/boxportation/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.boxportation.repository;
 import com.boxportation.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Optional<Item> findByCode(String code);
 }
