7679faa097beac5f719fca5693a2eb112b99f093
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-23 01:57:52 +0100

    Repo

--- src/main/java/com/boxportation/repository/BoxRepository.java
+++ src/main/java/com/boxportation/repository/BoxRepository.java
@@ -3,6 +3,11 @@ package com.boxportation.repository;
 import com.boxportation.model.Box;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoxRepository extends JpaRepository<Box, Long> {
 
+    Optional<Box> findByTxref(String txref);
+
+    boolean existsByTxref(String txref);
 }
