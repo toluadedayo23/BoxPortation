0a08eab0fe2c349e5db9e4d94e7a346d27273e57
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-23 17:03:03 +0100

    Mapper

--- src/main/java/com/boxportation/mapper/BoxMapper.java
+++ src/main/java/com/boxportation/mapper/BoxMapper.java
@@ -1,8 +1,16 @@
 package com.boxportation.mapper;
 
+import com.boxportation.dto.BoxResponses;
+
+import com.boxportation.model.Box;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 
 @Mapper(componentModel = "spring")
 public interface BoxMapper {
+
+
+    BoxResponses mapBoxToResponse(Box box);
 
 }
