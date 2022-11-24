f5ea3a77e1854661414e7110b4d709da1997d35d
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-24 06:43:20 +0100

    Controllers

--- src/main/java/com/boxportation/BoxPortationApplication.java
+++ src/main/java/com/boxportation/BoxPortationApplication.java
@@ -1,7 +1,12 @@
 package com.boxportation;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BoxPortationApplication {
@@ -9,5 +14,16 @@ public class BoxPortationApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoxPortationApplication.class, args);
     }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription,
+                                 @Value("${app.version}") String appVersion) {
+
+        return new OpenAPI().info(new Info().title("Box Transportation API").version(appVersion)
+                .description(appDescription).termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
+
 
 }
--- src/main/java/com/boxportation/controllers/BoxController.java
+++ src/main/java/com/boxportation/controllers/BoxController.java
@@ -1,9 +1,11 @@
 package com.boxportation.controllers;
 
 import com.boxportation.dto.BoxDto;
+import com.boxportation.dto.BoxResponses;
 import com.boxportation.dto.LoadBoxDto;
 import com.boxportation.dto.LoadedItemsResponse;
 import com.boxportation.services.BoxService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,36 +16,36 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/box")
+@RequestMapping("/api/box")
+@Tag(name = "Box Controller", description = "This REST controller provide services to manage Boxes in the Boxportation application")
 public class BoxController {
 
     private final BoxService boxService;
 
     @PostMapping
-    public ResponseEntity<String> registerBox(@RequestBody @Valid BoxDto boxDto){
+    public ResponseEntity<String> registerBox(@RequestBody @Valid BoxDto boxDto) {
         boxService.registerBox(boxDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Your Box has been created");
     }
 
     @PostMapping("/loadBox")
-    public ResponseEntity<LoadedItemsResponse> loadBox(@RequestBody @Valid LoadBoxDto loadBoxDto){
+    public ResponseEntity<LoadedItemsResponse> loadBox(@RequestBody @Valid LoadBoxDto loadBoxDto) {
         return new ResponseEntity<LoadedItemsResponse>(boxService.loadBox(loadBoxDto), HttpStatus.OK);
     }
 
     @GetMapping("/ListItemsInBox/{txref}")
-    public ResponseEntity<List<LoadedItemsResponse>>listItemsInBox(@PathVariable("txref") String txref){
+    public ResponseEntity<List<LoadedItemsResponse>> listItemsInBox(@PathVariable("txref") String txref) {
         return new ResponseEntity<List<LoadedItemsResponse>>(boxService.ListItemsInBox(txref), HttpStatus.OK);
     }
 
     @GetMapping("/getBoxesAvailableForLoading")
-    public ResponseEntity<String> getBoxesAvailableForLoading(){
-        boxService.availableBoxesForLoading();
-        return ResponseEntity.status(HttpStatus.CREATED).body("Your Box has been created");
+    public ResponseEntity<List<BoxResponses>> getBoxesAvailableForLoading() {
+        return ResponseEntity.status(HttpStatus.OK).body(boxService.availableBoxesForLoading());
 
     }
 
     @GetMapping("/checkBatteryLevel/{txref}")
-    public ResponseEntity<Double> checkBatteryLevel(@PathVariable("txref") String txref){
+    public ResponseEntity<Double> checkBatteryLevel(@PathVariable("txref") String txref) {
         return new ResponseEntity<Double>(boxService.BatteryLevel(txref), HttpStatus.OK);
     }
 
