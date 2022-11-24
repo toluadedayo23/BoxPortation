8795c23b2dde7454f74578c33bc231b5b24d67bf
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-24 06:49:02 +0100

    Exceptions

--- src/main/java/com/boxportation/exceptions/handler/ErrorResponse.java
+++ src/main/java/com/boxportation/exceptions/handler/ErrorResponse.java
@@ -1,9 +1,15 @@
 package com.boxportation.exceptions.handler;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.util.Date;
 
+@Getter
+@Setter
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
 public class ErrorResponse {
 
     private Date timestamp;
--- src/main/java/com/boxportation/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/boxportation/exceptions/handler/GlobalExceptionHandler.java
@@ -3,10 +3,17 @@ package com.boxportation.exceptions.handler;
 import com.boxportation.exceptions.BoxException;
 import com.boxportation.exceptions.ItemException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -32,5 +39,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("Item with the ITEM ID: {}, not found", itemException.getFieldValue());
 
         return new ErrorResponse(status, itemException.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        List<String> validationList = ex.getBindingResult().getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).collect(Collectors.toList());
+        log.error("Validation error: " + validationList);
+        return new ResponseEntity<>(new ErrorResponse(status, errorMessage), status);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleException(Exception e) {
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        log.error("Internal Error");
+
+        return new ErrorResponse(status, e.getMessage());
     }
 }
